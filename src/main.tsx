5e671e5178b0a529a3d754862daa0dc4e3ddc297
Ilona Andriyashyn <andriilo@fit.cvut.cz>
2023-03-05 15:47:58 +0100

    Installed MUI

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import theme from './helpers/theme'
+import { CssBaseline, ThemeProvider } from '@mui/material'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
