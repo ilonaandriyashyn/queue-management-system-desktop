885f337ea20e980a181fbe506c4cc26c5f6d21d8
Ilona Andriyashyn <andriilo@fit.cvut.cz>
2023-03-26 19:18:37 +0200

    Show alert when there is no counter or services

---
+++ src/components/NoCounterOrServiceAlert/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import Alert from '@mui/material/Alert'
+
+const NoCounterOrServiceAlert = () => {
+  return <Alert severity="warning">Pozor! Není zadaná přepážka či její služby.</Alert>
+}
+
+export default NoCounterOrServiceAlert
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,16 +1,30 @@
 import React from 'react'
 import Table from '../../components/Dashboard/Table'
 import TicketManager from '../../components/Dashboard/TicketManager'
+import NoCounterOrServiceAlert from '../../components/NoCounterOrServiceAlert'
+import { useAppSelector } from '../../store/hooks'
+import { selectCounterId, selectCounterServices } from '../../store/counter/slice'
 
 const styles = {
   wrapper: {
     height: '100%'
+  },
+  alert: {
+    marginTop: '1rem',
+    marginBottom: '2rem'
   }
 } as const
 
 function Dashboard() {
+  const counterId = useAppSelector(selectCounterId)
+  const services = useAppSelector(selectCounterServices)
   return (
     <div style={styles.wrapper}>
+      {(counterId === '' || services.length === 0) && (
+        <div style={styles.alert}>
+          <NoCounterOrServiceAlert />
+        </div>
+      )}
       <TicketManager />
       <Table />
     </div>
