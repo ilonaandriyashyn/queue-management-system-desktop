fdb88097ed86c8567dc7f1ccb87a048597017d1e
Ilona Andriyashyn <andriilo@fit.cvut.cz>
2023-04-10 15:07:09 +0200

    Added title to table

--- src/components/Dashboard/Table.tsx
+++ src/components/Dashboard/Table.tsx
@@ -5,34 +5,38 @@ import TableCell from '@mui/material/TableCell'
 import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
 import { type Ticket } from '../../types'
+import { Typography } from '@mui/material'
 
 const styles = {
-  table: {
-    mt: 16
+  wrapper: {
+    marginTop: '9em'
   },
   row: {
     '&:last-child td, &:last-child th': { border: 0 }
   }
-}
+} as const
 
 const Table = ({ tickets }: { tickets: Ticket[] }) => {
   return (
-    <MuiTable sx={styles.table}>
-      <TableHead>
-        <TableRow>
-          <TableCell>Číslo</TableCell>
-          <TableCell>Služba</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {tickets.map((ticket) => (
-          <TableRow key={ticket.id} sx={styles.row}>
-            <TableCell>{ticket.ticketNumber}</TableCell>
-            <TableCell>{ticket.service.name}</TableCell>
+    <div style={styles.wrapper}>
+      <Typography variant="h6">{'Aktuální fronta'}</Typography>
+      <MuiTable>
+        <TableHead>
+          <TableRow>
+            <TableCell>Číslo</TableCell>
+            <TableCell>Služba</TableCell>
           </TableRow>
-        ))}
-      </TableBody>
-    </MuiTable>
+        </TableHead>
+        <TableBody>
+          {tickets.map((ticket) => (
+            <TableRow key={ticket.id} sx={styles.row}>
+              <TableCell>{ticket.ticketNumber}</TableCell>
+              <TableCell>{ticket.service.name}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </MuiTable>
+    </div>
   )
 }
 
