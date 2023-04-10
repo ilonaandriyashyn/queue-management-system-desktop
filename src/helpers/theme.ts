1deb236accd0fd79df15559a4eaef783c6952c24
Ilona Andriyashyn <andriilo@fit.cvut.cz>
2023-04-10 14:05:08 +0200

    Deleted unused theme

--- src/helpers/theme.ts
+++
@@ -1,9 +0,0 @@
-import { createTheme } from '@mui/material'
-
-const theme = createTheme({
-  typography: {
-    // fontFamily: ['Roboto', 'normal'].join(',')
-  }
-})
-
-export default theme
--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,7 @@ import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
-import theme from './helpers/theme'
-import { CssBaseline, ThemeProvider } from '@mui/material'
+import { CssBaseline } from '@mui/material'
 import { Provider } from 'react-redux'
 import { store } from './store'
 import { QueryClient, QueryClientProvider } from 'react-query'
@@ -26,12 +25,10 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <SnackbarProvider>
-            <CssBaseline />
-            <App />
-          </SnackbarProvider>
-        </ThemeProvider>
+        <SnackbarProvider>
+          <CssBaseline />
+          <App />
+        </SnackbarProvider>
       </Provider>
     </QueryClientProvider>
   </React.StrictMode>
