53c1747d0e69ee9cad87c4a2b9a47f1cba531183
Ilona Andriyashyn <andriilo@fit.cvut.cz>
2023-04-10 13:39:45 +0200

    Exported server url to consts

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
+import { SERVER_URL } from '../helpers/consts'
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: SERVER_URL,
   timeout: 1000
 })
--- src/helpers/consts.tsx
+++ src/helpers/consts.tsx
@@ -2,6 +2,8 @@ import DashboardIcon from '@mui/icons-material/Dashboard'
 import SettingsIcon from '@mui/icons-material/Settings'
 import * as React from 'react'
 
+export const SERVER_URL = 'http://localhost:3000'
+
 export const PAGES = {
   DASHBOARD: {
     name: 'dashboard',
@@ -14,13 +16,7 @@ export const PAGES = {
 } as const
 
 export type PagesType = (typeof PAGES)[keyof typeof PAGES]['name']
-
-export const ORGANIZATION_ID = '9ca561dc-9442-4c46-b4b5-d9ba6950e281'
 export const OFFICE_ID = 'e22cd045-8ca7-43e5-a5c3-35e8563ccc3a'
-
-// TODO get this from state
-// export const COUNTER_ID = '007fc838-53cd-4e5a-8acd-7afeb5e77c97'
-// export const COUNTER_ID = 'b9d011be-c7cf-439d-a0c0-d6bcb6126ff4'
 
 export const API_URL = {
   OFFICE: `/offices/${OFFICE_ID}`,
